ab2aee78d427816c99c535bfc0d0b9affb2f8b94
unknown <ricardoarrecisrivera@gmail.com>
2025-02-22 21:17:38 -0600

    Se actualizan archivos

--- Persona.cpp
+++ Persona.cpp
@@ -1,7 +1,21 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-class Persona(){
-    
+class Persona{
+	// atributos
+	protected : string nombres,apellidos,direccion;
+				int telefono;
+	// costructor
+	protected : 
+		Persona(){
+		}
+		Persona(string nom,string ape,string dir,int tel){
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			telefono = tel;
+		}
+	
+	// metodos
+	void mostrar();
+	
 };
